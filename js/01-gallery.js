80e296cb5da44284617eaea82cb07ff1dcee93cc
Dmytro Chernomord <dchernomord@i-doit.com>
2023-10-16 11:22:21 +0200

    add tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+
+const createMarkup = (ref) => {
+  const images = galleryItems.map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+       class="gallery__image"
+       src="${preview}"
+       data-source="${original}"
+       alt="${description}"
+      />
+    </a>
+  </li>`}).join('')
+
+  ref.insertAdjacentHTML("afterbegin", images);
+}
+
+createMarkup(galleryRef);
+
+let lightBoxInstance;
+
+const handleEscape = ({key} = {}) => key === 'Escape' && lightBoxInstance?.close();
+
+const handleOnClick = (e) => {
+  e.preventDefault()
+
+  lightBoxInstance = basicLightbox.create(`<img alt="image" src="${e.target.dataset.source}" width="auto" height="auto">`, {
+    onShow: () => { document.addEventListener('keydown', handleEscape) },
+    onClose: () => { document.removeEventListener('keydown', handleEscape) },
+  })
+  lightBoxInstance.show();
+}
+
+galleryRef.addEventListener('click', handleOnClick);
+
+
